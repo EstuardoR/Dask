804439172904fc3d4165f6f11bea78d46b8bf965
Estuardo <rrodasestuardo2004@gmail.com>
2025-05-06 23:31:54 -0600

    style: Enhance layout component with Poppins font integration

--- dask-app/src/app/layout.tsx
+++ dask-app/src/app/layout.tsx
@@ -1,11 +1,18 @@
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <html lang="en">
-        <body>
-          {children}
-        </body>
-      </html>
-    );
-  }
-  
+  return (
+    <html lang="en" className={poppins.variable}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
