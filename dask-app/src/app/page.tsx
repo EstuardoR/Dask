399ef611118c25ba74ff36c115bd7d233397d6b5
Joan Estuardo <rrodasestuardo2004@gmail.com>
2025-07-27 23:07:25 -0600

    feature: main page, add todo card component

--- dask-app/src/app/page.tsx
+++ dask-app/src/app/page.tsx
@@ -1,11 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import layout from './layout';
 import styles from '../page.module.css'
+import TodoCard from './components/todo-card/todo-card';
 
 export default function Home() {
   return (
     <div className={styles.background}>
       <div className={styles.title}>
-        <h1>Let's work</h1>
+        <h1>Let`s work</h1>
+      </div>
+
+      <div className={styles.taskList} >
+        <TodoCard text='Hacer tarea de algoritmos :) !' />
+        <TodoCard text='Limpiar mi escritorio' />
+        <TodoCard text='Sacar a pasear a mi perro' />
       </div>
 
     </div>
